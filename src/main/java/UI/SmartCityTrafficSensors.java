fa706b6553b65334b665f7b462444b27e9fe7e76
Helena Polleros <helena.polleros@htlpinkafeld.at>
2026-06-17 12:05:52 +0200

    export fixed

--- src/main/java/UI/SmartCityTrafficSensors.java
+++ src/main/java/UI/SmartCityTrafficSensors.java
@@ -1,5 +1,6 @@
 package UI;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +15,8 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 @Table(name = "SmartCityTrafficSensors")
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 
 public class SmartCityTrafficSensors {
 
